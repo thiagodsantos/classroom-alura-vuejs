2307c69c32626f6a80d2378e40c6ccf25b315b57
Thiago Alexandre <thiagoalexandre@pecege.com>
2019-11-25 17:51:54 -0300

    Iterando com v-for

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>{{ titulo }}</h1>
-    <img :src="foto.url" :alt="foto.titulo">
+    <ul>
+      <li v-for="foto of fotos" v-bind:key="foto.id">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -10,10 +14,18 @@ export default {
   data () {
     return {
       titulo: 'Galeria',
-      foto: {
-        url: 'https://cdn.pixabay.com/photo/2018/08/21/23/29/fog-3622519__340.jpg',
-        titulo: 'Floresta'
-      }
+      fotos: [
+        {
+          id: 1,
+          url: 'https://cdn.pixabay.com/photo/2018/08/21/23/29/fog-3622519__340.jpg',
+          titulo: 'Floresta'
+        },
+        {
+          id: 2,
+          url: 'https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832__340.jpg',
+          titulo: 'Island'
+        }
+      ]
     }
   }
 }
